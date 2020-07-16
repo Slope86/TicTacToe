748ff794345d8a3a18eb3ebd6eb3a918e391cecd
Slope123 <60516263+Slope123@users.noreply.github.com>
2020-07-16 17:39:42 +0800

    Update GameCanvas.h

--- GameCanvas.h
+++ GameCanvas.h
@@ -2,8 +2,8 @@
 #define GAMECANVAS_H
 #include <opencv2/opencv.hpp>
 
-enum class Piece { human, ai, none};
-enum class Winner { human, ai, draw, uncertain};
+enum class Piece { human, bot, none};
+enum class Winner { human, bot, draw, uncertain};
 
 // move of the piece, for exsample:
 // Move(0,0)    Move(1,0)    Move(2,1)
@@ -23,25 +23,27 @@ public:
 class GameCanvas
 {
 private:
-    const int def_width = 600;
-    const int def_high = 760; // The default resolution that the game designed with
+    const int def_width = 600; // The default resolution of the game-design
+    const int def_high = 760; // The default resolution of the game-design
     int width = 600; // The actual display width of the game (change base on user's screen width)
+    std::string windowName = "Tie Tac Toe"; // The name of the game window
     Piece board[3][3]; // Record what pieces are on the board
     cv::Mat canvas; // The canvas where we draw all the text, game board, piece
     Winner winner; // Record the winner of current game (use to determine whether the game is still progress or already end)
-    bool nextMoveAI; // If human moved first and draw in the last game,computer move first in the next game
+    bool nextMoveBOT; // If human moved first and draw in the last game,bot move first in the next game
 
 public:
+    GameCanvas(std::string name); //input : what name you want to use as the window name. (default:Tie Tac Toe)
     GameCanvas();
 
     // Main game funcion, get and check player's input then progress one round then wait for next input
     // Input: coordinate where player click on the game display window
     void playerInput(int,int);
 
-    //get human/ai's move, place the piece on the board and draw it on the canvas
+    //get human/bot's move, place the piece on the board and draw it on the canvas
     //check whether there is a winner, if so then end the game
     void humanMove(Move);
-    void aiMove();
+    void botMove();
 
     // Check and return the winner of current board, if no winner yet, return "Winner::uncertain"
     Winner checkWinner();
@@ -55,7 +57,7 @@ public:
     // Game end, draw outcome of the game and wait user to start a newgame
     void gameEnd();
 
-    // Returns ai's best move via the minimax algorithm. If there are multiple best moves, randomly return one
+    // Returns bot's best move via the minimax algorithm. If there are multiple best moves, randomly return one
     Move miniMax();
 
     // miniMax algorithm's sub function, return the score of current choice move
